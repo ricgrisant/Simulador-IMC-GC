4b14e2e330db78dd574732cef9a6ed77ba70316e
Ricardo <ricardogrijalva10@gmail.com>
2020-08-10 01:49:22 -0600

    formulario y datos

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -2,67 +2,80 @@ import React, {Component} from 'react';
 
 class Formulario extends Component {
 
-     // refs son para leer los valores de los campos de un formulario
-     refNombrePersona = React.createRef();
-     refIdPersona = React.createRef();
-     refEstatura = React.createRef();
-     refPeso = React.createRef();
-     refCirCintura = React.createRef();
-     refCirCuello = React.createRef();
-     refCirCaderaMuj = React.createRef();
-     refGenero = React.createRef();
 
-     calcularGrasa= (e) =>  {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+        nombrePersona : '', 
+        idPersona : '',
+        estatura : '', 
+        peso : '',
+        cirCintura : '',
+        cirCuello : '',
+        cirCaderaMuj : '',
+        generoMujer : '',
+        generoHombre : ''
+      }
+    }
+
+    handleFormulario = (e) => {
+      this.setState({
+        [e.target.id] : e.target.value
+      })
+    }
+
+     handleSubmit = (e) =>  {
           e.preventDefault();
 
           // leer el género
-          const genero = this.refGenero.current.checked ? 'hombre' : 'mujer';
+          console.log(this.state);
 
           // e.currentTarget.reset() 
      }
 
      render() {
           return (
-              <form className="cotizar-auto" onSubmit={this.calcularGrasa} >
+              <form className="cotizar-auto" onSubmit={this.handleSubmit} >
                   <div className="campo">
                     <label>Nombre: </label>
-                    <input ></input>
+                    <input id="nombrePersona" onChange={this.handleFormulario}></input>
                   </div>
 
                   <div className="campo">
                     <label>ID Persona: </label>
-                    <input ></input>
+                    <input id="idPersona" onChange={this.handleFormulario}></input>
                   </div>
 
                   <div className="campo">
                     <label>Estatura (cm): </label>
-                    <input ></input>
+                    <input id="estatura" onChange={this.handleFormulario}></input>
                   </div>
 
                   <div className="campo">
                     <label>Peso (kg): </label>
-                    <input ></input>
+                    <input id="peso" onChange={this.handleFormulario}></input>
                   </div>
 
                   <div className="campo">
                     <label>Circunferencia Cintura: </label>
-                    <input ></input>
+                    <input id="cirCintura" onChange={this.handleFormulario}></input>
                   </div>
 
                   <div className="campo">
                     <label>Circunferencia Cuello: </label>
-                    <input ></input>
+                    <input id="cirCuello" onChange={this.handleFormulario}></input>
                   </div>
 
                   <div className="campo">
                     <label>Circ. Cadera (Sólo Mujer): </label>
-                    <input ></input>
+                    <input id="cirCaderaMuj" onChange={this.handleFormulario}></input>
                   </div>
   
                   <div className="campo">
                       <label>Género:</label>
-                      <input type="radio" ref={this.refGeneroMujer}  name="plan" value="mujer"/> Mujer
-                      <input type="radio" ref={this.refGeneroHombre}  name="plan" value="hombre"/> Hombre
+                      <input type="radio" id="generoMujer" onChange={this.handleFormulario}  name="plan" value="mujer"/> Mujer
+                      <input type="radio" id="generoHombre" onChange={this.handleFormulario}  name="plan" value="hombre"/> Hombre
                   </div>
   
                   <button type="submit" className="boton">Calcular</button>
